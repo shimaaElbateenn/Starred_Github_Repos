37a4890f884838cebf006044ba49b35bdedf42f0
shimaa <shimaa.elbateen.se@gmail.com>
2021-09-20 23:41:56 +0200

    Initial commit

---
+++ app/src/main/java/com/example/starredgithubrepos/ui/MainActivity.java
@@ -0,0 +1,16 @@
+package com.example.starredgithubrepos.ui;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.starredgithubrepos.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
